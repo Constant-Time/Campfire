873eec2e914acea0fb68d14f77f030bec3321e2e
Caleb Kress <calebkress@gmail.com>
2018-06-26 16:12:14 -0500

    updated InputField from createClass syntax to Component syntax

--- react-client/src/components/story-page/inputField.jsx
+++ react-client/src/components/story-page/inputField.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 var max_chars = 250;
 
-var InputField = React.createClass({
-	getInitialState: function() {
-    	return {
-        	chars_left: max_chars
-        };
-    },
-    handleChange(event) {
-    	var input = event.target.value;
-        this.setState({
-        	chars_left: max_chars - input.length
-        });
-    },
-	render: function() {
+class InputField extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { chars_left: max_chars };
+		this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+  	var input = event.target.value;
+    this.setState({
+    	chars_left: max_chars - input.length
+    });
+  }
+
+	render() {
     	return (
 					<div>
             	<textarea id="NewStoryText" rows="5" cols="50" onChange={this.handleChange}></textarea>
@@ -22,6 +25,6 @@ var InputField = React.createClass({
             </div>
         );
     }
-});
+};
 
 export default InputField;
